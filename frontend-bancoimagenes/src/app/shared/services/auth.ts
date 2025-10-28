d7e6c99432475b9a63666c918d35d59fb294281f
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-28 16:28:03 -0600

    autenticacion

--- frontend-bancoimagenes/src/app/shared/services/auth.ts
+++ frontend-bancoimagenes/src/app/shared/services/auth.ts
@@ -1,8 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
+export class AuthServicw {
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient, private router: Router){}
+
+  login(credentials: { 
+    username: string; 
+    password:string}){
+      return this.http.post<{ token: string}>(`${this.apiUrl}/login`, credentials);
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  getToken():string | null {
+    return localStorage.getItem('token');
+  }
+
+  getRole(): string | null {
+    const token = this.getToken();
+    if(!token) return null;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.role || null;
+  }
   
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
 }
